b6184b358b0591662d7a60927e9bb105c72c31a4
maxpolski <maxpolski@nixsolutions.com>
2016-05-23 23:16:00 +0300

    tiny codestyle fixes + optimized month search

--- js/scripts.js
+++ js/scripts.js
@@ -5,72 +5,44 @@ var commentTextInput = document.getElementById('commentText');
 addCommentBtn.onclick = addCommentHandler;
 
 document.body.onkeydown = function(e) {
-    if (e.ctrlKey && e.keyCode === 13)
-        addCommentHandler(e);
+  if (e.ctrlKey && e.keyCode === 13) {
+    addCommentHandler(e);
+  }
 };
-
-
 
 function addCommentHandler(e) {
 	e.preventDefault();
-	var commentText = commentTextInput.value;
+
+  var commentText = commentTextInput.value;
 
 	renderCommentBlock(commentText);
-
 	commentTextInput.value = '';
 }
 
-function dateNow() {
+function getDate() {
+  var monthes = [
+    'январь',
+    'февраль',
+    'март',
+    'апрель',
+    'май',
+    'июнь',
+    'июль',
+    'август',
+    'сентябрь',
+    'октябрь',
+    'ноябрь',
+    'декабрь'
+  ];
   var date = new Date();
 
   var dd = date.getDate();
   if (dd < 10) dd = '0' + dd;
-
-  var mm = date.getMonth();
-  switch(mm) {
-  	case 0:
-  		mm = 'январь';
-  		break;
-  	case 1:
-  		mm = 'февраль';
-  		break;
-  	case 2:
-  		mm = 'март';
-  		break;
-  	case 3:
-  		mm = 'апрель';
-  		break;
-  	case 4:
-  		mm = 'май';
-  		break;
-  	case 5:
-  		mm = 'июнь';
-  		break;
-  	case 6:
-  		mm = 'июль';
-  		break;
-  	case 7:
-  		mm = 'август';
-  		break;
-  	case 8:
-  		mm = 'сентябрь';
-  		break;
-  	case 9:
-  		mm = 'октябрь';
-  		break;
-  	case 10:
-  		mm = 'ноябрь';
-  		break;
-  	case 11:
-  		mm = 'декабрь';
-  		break;
-  }
-
+  var mm = monthes[date.getMonth()];
   var yy = date.getFullYear();
 
   return dd + ' ' + mm + ' ' + yy;
 }
-
 
 function renderCommentBlock(commentText) {
 	var commentBlock = document.createElement('DIV');
@@ -80,7 +52,7 @@ function renderCommentBlock(commentText) {
 		commentName.innerHTML = '%userName%';
 
 	var commentDate = document.createElement('SPAN');
-		commentDate.innerHTML = dateNow();
+		commentDate.innerHTML = getDate();
 
 	var commentParagraph = document.createElement('P');
 		commentParagraph.classList.add('comment');
@@ -92,5 +64,3 @@ function renderCommentBlock(commentText) {
 	commentBlock.appendChild(commentDate);
 	commentBlock.appendChild(commentParagraph);
 }
-
-
